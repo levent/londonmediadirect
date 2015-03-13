1b8f49b4e8772842ecdff09f60560af798f21e13
Levent Ali <lebreeze@gmail.com>
2015-03-13 17:20:59 +0000

    Moved repo again

--- Gruntfile.js
+++ Gruntfile.js
@@ -280,7 +280,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'git@github.com:londonmediadirect/londonmediadirect.github.io.git',
+          remote: 'git@github.com:londonmediadirect/londonmediadirect.git',
           branch: 'gh-pages',
           commit: true,
           push: true
